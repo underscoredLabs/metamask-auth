02e35eed4d858b3ac34b2d7031c6d911daacdf9a
leon-do <leondo123@gmail.com>
2021-02-20 11:59:20 -0500

    clean up

--- handleLogin.js
+++ handleLogin.js
@@ -2,12 +2,9 @@ function handleLogin() {
   if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider);
     window.ethereum.enable();
-
     window.ethereum.on("accountsChanged", function (accounts) {
       location.reload();
     });
-
-    return true;
   } else {
     const html = `
       <h1>No Metamask</h1>
@@ -15,6 +12,5 @@ function handleLogin() {
       <img class="signButton" src="/images/install.png" onclick="location.href='https://metamask.io'">
     `;
     document.getElementById("app").innerHTML = html;
-    return false;
   }
 }
